8bf415aa435083fef25709cb3b4c6e214674583f
OmShinde-23 <omwindows2003@gmail.com>
2024-02-02 11:26:24 +0530

    successful implementation of evently

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,19 +1,24 @@
 import Collection from '@/components/shared/Collection'
 import { Button } from '@/components/ui/button'
 import { getEventsByUser } from '@/lib/actions/event.actions'
+import { getOrdersByUser } from '@/lib/actions/order.actions'
 import { IOrder } from '@/lib/database/models/order.model'
+import { SearchParamProps } from '@/types'
 import { auth } from '@clerk/nextjs'
 import Link from 'next/link'
 import React from 'react'
 
-const ProfilePage = async () => {
+const ProfilePage = async ({searchParams}: SearchParamProps) => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
 
-  const orders =await getEventsByUser({userId, page: 1})
+  const ordersPage = Number(searchParams?.ordersPage) || 1;
+  const eventsPage = Number(searchParams?.eventsPage) || 1;
 
-  const orderedEvents = orders?.data?.map((order: IOrder) => order.event) || [];
-  const OrganizedEvents = await getEventsByUser({userId, page: 1})
+  const orders =await getOrdersByUser({userId, page: ordersPage})
+
+  const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
+  const OrganizedEvents = await getEventsByUser({userId, page: eventsPage})
 
   console.log({OrganizedEvents})
 
@@ -38,9 +43,9 @@ const ProfilePage = async () => {
         emptyStateSubtext="No worries - plenty of exciting events to explore!"
         collectionType="My_Tickets"
         limit={3}
-        page={1}
+        page={ordersPage}
         urlParamName="ordersPage"
-        totalPages={2}
+        totalPages={orders?.totalPages}
       />
     </section>
 
@@ -63,9 +68,9 @@ const ProfilePage = async () => {
         emptyStateSubtext="Go create some now"
         collectionType="Events_Organized"
         limit={6}
-        page={1}
+        page={eventsPage}
         urlParamName="eventsPage"
-        totalPages={2}
+        totalPages={OrganizedEvents?.totalPages}
       />
     </section>
     </>
